894f8617cac37fa79489a81c25f443e4919dfac9
Ethan Harsh <ethan@ethanharsh.com>
2022-04-11 12:43:01 -0400

    Add Medium/Occupation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,6 +29,7 @@ function App ()
           <div style={ { padding: '5rem' } }>
             <Typography variant="h1" className="animate__animated animate__fadeInUp">Hello,</Typography>
             <Typography variant="h2" className="animate__animated animate__fadeInUp">My name is Ethan.</Typography>
+            <Typography variant="h2" className="animate__animated animate__fadeInUp">I am a Web3 Developer.</Typography>
             <Box sx={ { display: 'inline-flex', flexDirection: 'row', justifyContent: 'space-evenly', marginTop: '0.5rem' } } className="animate__animated animate__fadeIn">
               <Link href="https://github.com/EthanHarsh" color="inherit">
                 <GitHubIcon fontSize='large' sx={ { marginRight: '20px', fontSize: '3rem' } } />
@@ -53,7 +54,7 @@ function App ()
               <BottomNavigationAction label="Home" icon={ <HomeIcon /> } />
               <BottomNavigationAction label="Development" icon={ <CodeIcon /> } href="https://github.com/EthanHarsh" />
               <BottomNavigationAction label="ClientHub" icon={ <HubIcon /> } href="https://secure.ethanharsh.com" />
-              <BottomNavigationAction label="Soon" icon={ <LibraryBooksIcon /> } disabled color="disabled" />
+              <BottomNavigationAction label="Writing" icon={ <LibraryBooksIcon /> } href="https://ethanharsh.medium.com/" />
             </BottomNavigation>
           </Paper>
         </main>
